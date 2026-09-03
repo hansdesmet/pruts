a27f8d49d82d0e8bf1ef08cadb6ece500eeb734f
Hans Desmet <hans.desmet@vdab.be>
2026-09-03 08:35:25 +0200

    public weg

--- src/main/java/be/vdab/pruts/PrutsApplication.java
+++ src/main/java/be/vdab/pruts/PrutsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PrutsApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(PrutsApplication.class, args);
     }
 

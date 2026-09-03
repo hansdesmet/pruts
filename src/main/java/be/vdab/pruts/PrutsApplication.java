50d4075ff6acf13bca00094ea9e3cbbe1ea452e5
Hans Desmet <hans.desmet@vdab.be>
2026-09-03 08:20:10 +0200

    initial commit

---
+++ src/main/java/be/vdab/pruts/PrutsApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.pruts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrutsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrutsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/pruts/PrutsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.pruts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrutsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
